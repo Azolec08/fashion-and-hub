ce2744275f6f04b5aa8b4b19e974045293b01609
Mark Celoza <markangeloceloza380@gmail.com>
2024-08-23 17:54:45 +0800

    first commit

---
+++ src/features/footer.tsx/components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="h-[50px] w-full flex items-center justify-center">
+      &copy; Food and Fashion Hub
+    </div>
+  );
+};
+
+export default Footer;
